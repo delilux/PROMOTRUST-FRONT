f35863e331d15eeb7fb3fb85a60d90007929995d
MafiiVr <vallejosr.mafer@gmail.com>
2024-10-21 19:54:04 -0500

    update services tips

--- src/app/services/tips.service.ts
+++ src/app/services/tips.service.ts
@@ -4,3 +4,14 @@ import { Observable } from 'rxjs';
 import { Tips } from '../models/tips';
 import { environment } from '../../environments/environment';
 
+const base_url = environment.base
+@Injectable({
+    providedIn:'root'
+})
+export class TipsService{
+    private url=`${base_url}/tips`;
+    constructor(private http: HttpClient){}
+    list():Observable<Tips[]>{
+        return this.http.get<Tips[]>(this.url);
+    }
+}

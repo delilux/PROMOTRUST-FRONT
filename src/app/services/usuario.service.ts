570f070b2e196ae93f41f1a47635d1f5e6528773
MafiiVr <vallejosr.mafer@gmail.com>
2024-10-21 21:26:48 -0500

    Create usuario.service.ts

---
+++ src/app/services/usuario.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment';
+import { HttpClient } from '@angular/common/http';
+import { Usuario } from '../models/Usuario';
+const base_url = environment.base;
+@Injectable({
+  providedIn: 'root'
+})
+export class UsuarioService {
+  private url = `${base_url}/usuarios`;
+
+  constructor(private http: HttpClient) {}
+  list() {
+    return this.http.get<Usuario[]>(this.url)
+  }
+}

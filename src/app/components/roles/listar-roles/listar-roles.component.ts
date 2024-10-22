85ab4acf7403667cb136bf30de1b3a7ab5265b9e
MafiiVr <vallejosr.mafer@gmail.com>
2024-10-21 21:28:05 -0500

    Create listar-roles.component.ts

---
+++ src/app/components/roles/listar-roles/listar-roles.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-listar-roles',
+  standalone: true,
+  imports: [],
+  templateUrl: './listar-roles.component.html',
+  styleUrl: './listar-roles.component.css'
+})
+export class ListarRolesComponent {
+
+}

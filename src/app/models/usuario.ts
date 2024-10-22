aef229562b27adc14d197be0f8d28efc32585fd9
MafiiVr <vallejosr.mafer@gmail.com>
2024-10-21 21:27:00 -0500

    Update usuario.ts

--- src/app/models/usuario.ts
+++ src/app/models/usuario.ts
@@ -1,1 +1,12 @@
-export class Usuario {}
+import { Tips } from "./Tips";
+
+export class Usuario {
+     id: number=0;
+     nombre:string="";
+     contrasenia:string="";
+     apellidos:string="";
+     telefono:number=0;
+     correo:string="";
+     RUC:string="";
+     TipsDTO:Tips=new Tips;
+}

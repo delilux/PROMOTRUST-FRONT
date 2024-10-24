0f316e17811d55af920c28c5bc27b14d37ee1921
delilux <142470442+delilux@users.noreply.github.com>
2024-10-24 16:54:48 -0500

    Revert "Create registrarcontratousuario.component.ts"
    This reverts commit 82ec81ce31b728fb7414fa8228d0c515360f0eee.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,29 +1,9 @@
 import { Routes } from '@angular/router';
 import { ContratousuarioComponent } from './components/contratousuario/contratousuario.component';
-import { InicioComponent } from './components/inicio/inicio.component';
-import { ContratoComponent } from './components/contrato/contrato.component';
-import { RegistrarcontratousuarioComponent } from './components/contratousuario/registrarcontratousuario/registrarcontratousuario.component';
-import { MetricasComponent } from './components/metricas/metricas.component';
 
 
 export const routes: Routes = [
-    //se adaptará luego
     {
-        path: '', redirectTo: 'inicio', pathMatch: 'full' // Ruta predeterminada que redirige a 'inicio'
-    },
-    {
-        path:'contratousuario',component:ContratousuarioComponent,
-    },
-    {
-        path:'inicio',component:InicioComponent
-    },
-    {
-        path:'contrato', component:ContratoComponent
-    },
-    {
-        path:'fechacontrato',component:RegistrarcontratousuarioComponent
-    },
-    {
-        path:'vermetricas',component:MetricasComponent
+        path:'contratousuario',component:ContratousuarioComponent
     }
 ];

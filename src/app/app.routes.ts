d9940e5680306cdeb886380809bb237a8fb7e87c
delilux <142470442+delilux@users.noreply.github.com>
2024-10-24 17:05:14 -0500

    app config

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,26 @@
 import { Routes } from '@angular/router';
 import { ContratousuarioComponent } from './components/contratousuario/contratousuario.component';
+import { MetricasComponent } from './components/metricas/metricas.component';
+import { ContratoComponent } from './components/contrato/contrato.component';
+import { InicioComponent } from './components/inicio/inicio.component';
 
 
 export const routes: Routes = [
     {
+        path: '', redirectTo: '/bienvenido', pathMatch: 'full'
+    },
+    {
+        path:'bienvenido',component:InicioComponent
+    },
+    {
         path:'contratousuario',component:ContratousuarioComponent
-    }
+    },
+    {
+        path:'vermetricas',component:MetricasComponent
+    },
+    {
+        path:'contrato',component:ContratoComponent
+    },
+
+
 ];

dfcf52064909969c20f8580b2245d04118fa7b94
delilux <142470442+delilux@users.noreply.github.com>
2024-10-24 16:53:05 -0500

    Update contrato.component.ts

--- src/app/components/contrato/contrato.component.ts
+++ src/app/components/contrato/contrato.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute, RouterOutlet } from '@angular/router';
+import { RegistrarcontratoComponent } from './registrarcontrato/registrarcontrato.component';
 
 @Component({
   selector: 'app-contrato',
   standalone: true,
-  imports: [],
+  imports: [RegistrarcontratoComponent, RouterOutlet],
   templateUrl: './contrato.component.html',
   styleUrl: './contrato.component.css'
 })

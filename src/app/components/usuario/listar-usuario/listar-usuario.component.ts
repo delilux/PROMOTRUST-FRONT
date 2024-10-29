b6bd9b8ddb779ebdf9c5aa39f72ed627c27ab859
MafiiVr <vallejosr.mafer@gmail.com>
2024-10-29 01:05:28 -0500

    Update listar-usuario.component.ts

--- src/app/components/usuario/listar-usuario/listar-usuario.component.ts
+++ src/app/components/usuario/listar-usuario/listar-usuario.component.ts
@@ -1,12 +1,39 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { Usuario } from '../../../models/usuario';
+import { UsuarioService } from '../../../services/usuario.service';
 
 @Component({
   selector: 'app-listar-usuario',
   standalone: true,
-  imports: [],
+  imports: [MatTableModule,
+    MatPaginatorModule,
+    MatCardModule,
+    MatButtonModule,
+    MatIconModule,
+    CommonModule,
+    RouterLink],
   templateUrl: './listar-usuario.component.html',
   styleUrl: './listar-usuario.component.css'
 })
-export class ListarUsuarioComponent {
+export class ListarUsuarioComponent implements OnInit {
+  displayedColumns: string[] = ['nombre', 'apellidos', 'RUC', 'telefono', 'correo'];
+  dataSource: MatTableDataSource<Usuario> = new MatTableDataSource();
+  constructor(private uS: UsuarioService) {}
+  ngOnInit(): void {
+    this.uS.list().subscribe((data) => {
+      this.dataSource = new MatTableDataSource(data);
+    });
+
+    this.uS.getList().subscribe((data) => {
+      this.dataSource = new MatTableDataSource(data);
+    });
+  }
 
 }

b1c9db74656da8dcc84003ed711cd452ca809974
MafiiVr <vallejosr.mafer@gmail.com>
2024-11-04 00:27:36 -0500

    Update tips.component.ts

--- src/app/components/tips/tips.component.ts
+++ src/app/components/tips/tips.component.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, RouterOutlet } from '@angular/router';
+import { ListarTipsComponent } from './listar-tips/listar-tips.component';
 
 @Component({
   selector: 'app-tips',
   standalone: true,
-  imports: [],
+  imports: [RouterOutlet, ListarTipsComponent],
   templateUrl: './tips.component.html',
   styleUrl: './tips.component.css'
 })
-export class TipsComponent {
+export class TipsComponent implements OnInit {
+  constructor(public route:ActivatedRoute){}
+
+    
+  ngOnInit(): void {
+    
+  }
 
 }

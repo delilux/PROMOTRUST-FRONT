1ceab5526c7376bcdf61cb2ec7e25163d1abf798
MafiiVr <vallejosr.mafer@gmail.com>
2024-11-04 00:27:55 -0500

    Update tips.service.ts

--- src/app/services/tips.service.ts
+++ src/app/services/tips.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, Subject } from 'rxjs';
 import { environment } from '../../environments/environment';
 import { Tips } from '../models/Tips';
 
@@ -10,8 +10,30 @@ const base_url = environment.base
 })
 export class TipsService{
     private url=`${base_url}/tips`;
-    constructor(private http: HttpClient){}
-    list():Observable<Tips[]>{
-        return this.http.get<Tips[]>(this.url);
-    }
+    private listacambio = new Subject<Tips[]>();
+
+  constructor(private http: HttpClient) {}
+  list() {
+    return this.http.get<Tips[]>(this.url)
+  }
+  insert(ro:Tips){
+    return this.http.post(this.url,ro);
+  }
+  setList(listaNueva:Tips[]){
+    this.listacambio.next(listaNueva)
+  }
+  getList(){
+    return this.listacambio.asObservable();
+  }
+  delete(id: number) {
+    return this.http.delete(`${this.url}/${id}`);
+  }
+
+  listId(id: number) {
+    return this.http.get<Tips>(`${this.url}/${id}`);
+  }
+
+  update(r: Tips) {
+    return this.http.put(this.url, r);
+  }
 }

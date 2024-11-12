ead2cc7aee10d161e9cf31a5dbfe35e0a7fd7a59
MafiiVr <vallejosr.mafer@gmail.com>
2024-11-12 10:04:00 -0500

    Update app.config.ts

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,7 +3,7 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
-import { provideHttpClient, withFetch } from '@angular/common/http';
+import { provideHttpClient, withFetch, withInterceptorsFromDi } from '@angular/common/http';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { JwtModule } from '@auth0/angular-jwt';
 export function tokenGetter() {
@@ -16,9 +16,7 @@ export const appConfig: ApplicationConfig = {
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideClientHydration(),
-    provideHttpClient(withFetch()),
-    provideAnimationsAsync(),
-    provideAnimationsAsync(),
+    provideHttpClient(withInterceptorsFromDi()), 
     provideAnimationsAsync(),
     importProvidersFrom(
       JwtModule.forRoot({

1941ed3c52fbf47165695c6a4bb02d8a4ef92d9c
MafiiVr <vallejosr.mafer@gmail.com>
2024-11-11 19:14:34 -0500

    Update app.config.ts

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,30 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import { JwtModule } from '@auth0/angular-jwt';
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideHttpClient, withFetch } from '@angular/common/http';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-
+export function tokenGetter() {
+  return sessionStorage.getItem('token');
+}
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }),
-     provideRouter(routes), provideClientHydration(), provideHttpClient(withFetch()), provideAnimationsAsync(), provideAnimationsAsync(), provideAnimationsAsync()]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideClientHydration(),
+    provideHttpClient(withFetch()),
+    provideAnimationsAsync(),
+    importProvidersFrom(
+      JwtModule.forRoot({
+        config: {
+          tokenGetter: tokenGetter,
+          allowedDomains: ['localhost:8081'],
+          disallowedRoutes: ['http://localhost:8081/login/forget'],
+        },
+      })
+    )
+    
+   
+  ],
 };

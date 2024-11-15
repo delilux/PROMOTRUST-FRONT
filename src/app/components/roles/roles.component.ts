bf07376fb17420965886c925e4b54c2a5a761c8a
MafiiVr <vallejosr.mafer@gmail.com>
2024-11-15 18:48:37 -0500

    Update roles.component.ts

--- src/app/components/roles/roles.component.ts
+++ src/app/components/roles/roles.component.ts
@@ -1,12 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ListarRolesComponent } from './listar-roles/listar-roles.component';
+import { ActivatedRoute, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-roles',
   standalone: true,
-  imports: [],
+  imports: [ListarRolesComponent, RouterOutlet],
   templateUrl: './roles.component.html',
   styleUrl: './roles.component.css'
 })
-export class RolesComponent {
+export class RolesComponent implements OnInit {
+  constructor(public route: ActivatedRoute) { }
+  ngOnInit(): void {
+
+  }
 
 }

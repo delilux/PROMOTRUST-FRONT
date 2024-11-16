02e2781b11626d7ffb815bc8899f923f2c6486e8
MafiiVr <vallejosr.mafer@gmail.com>
2024-11-16 01:18:18 -0500

    Update activo.component.ts

--- src/app/components/reportes/activo/activo.component.ts
+++ src/app/components/reportes/activo/activo.component.ts
@@ -1,12 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UsuarioContratoActivoDTO } from '../../../models/UsuarioContratoActivoDTO';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+import { UsuarioService } from '../../../services/usuario.service';
+import { MatButtonModule } from '@angular/material/button';
+import { RouterLink } from '@angular/router';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-activo',
   standalone: true,
-  imports: [],
+  imports: [MatTableModule, 
+    MatButtonModule, 
+    RouterLink,
+    MatFormFieldModule,
+    MatPaginatorModule,
+    MatInputModule,
+    MatIconModule,
+    CommonModule],
   templateUrl: './activo.component.html',
   styleUrl: './activo.component.css'
 })
-export class ActivoComponent {
+export class ActivoComponent implements OnInit {
+  displayedColumns: string[] = [
+    'id',
+    'tipo_evento',
+    'correo',
+    'telefono',
+    'detalle_contrato',
+    'estadocontrato'
+    
+
+  ];
+  dataSource: MatTableDataSource<UsuarioContratoActivoDTO> = new MatTableDataSource();
+  constructor(private eS:UsuarioService) {}
+  ngOnInit(): void {
+    this.eS.getActivo().subscribe((data) => {
+      this.dataSource = new MatTableDataSource(data);  
+      
+    });
+  }
 
 }

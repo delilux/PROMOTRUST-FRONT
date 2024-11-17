958db672b7b9082280f17e44955b07703b970d8b
delilux <142470442+delilux@users.noreply.github.com>
2024-11-17 07:39:20 -0500

    Update creaeditaservicios.component.ts

--- src/app/components/servicios/creaeditaservicios/creaeditaservicios.component.ts
+++ src/app/components/servicios/creaeditaservicios/creaeditaservicios.component.ts
@@ -33,95 +33,110 @@ import { ContratoService } from '../../../services/contrato.service';
     CommonModule,
     MatFormFieldModule,
   ],
-  templateUrl: './creaeditaservicios.component.html',   
-  styleUrl: './creaeditaservicios.component.css'
+  templateUrl: './creaeditaservicios.component.html',
+  styleUrl: './creaeditaservicios.component.css',
 })
-export class CreaeditaserviciosComponent implements OnInit{
+export class CreaeditaserviciosComponent implements OnInit {
   form: FormGroup = new FormGroup({});
   servicios: Servicios = new Servicios();
   id: number = 0;
   edicion: boolean = false;
+  listacontrato: Contrato[] = [];// Arreglo de contratos
 
   listaCategoria: { value: string; viewValue: string }[] = [
-    { value: 'MuyBuena', viewValue: 'MuyBuena' },
-    { value: 'Buena', viewValue: 'Buena' },
-    { value: 'Mala', viewValue: 'mala' },
-    { value: 'MuyMala', viewValue: 'MuyMala' },
+    { value: 'MuyBuena', viewValue: 'Promocion marca' },
+    { value: 'Buena', viewValue: 'Publicacion sobre marca' },
+    { value: 'Mala', viewValue: 'Historia para marca' },
+    { value: 'MuyMala', viewValue: 'Video para marca' },
   ];
   listaEstado: { value: string; viewValue: string }[] = [
-    { value: 'No_Disponible', viewValue: 'No_Disponible' },
-    { value: 'Disponible', viewValue: 'Disponible' },
-    { value: 'SinInformacion', viewValue: 'SinInformacion' },
-   
+    { value: 'No_Disponible', viewValue: 'Activo' },
+    { value: 'Disponible', viewValue: 'En curso' },
+    { value: 'SinInformacion', viewValue: 'Finalizado' },
   ];
 
-  listacontrato:Contrato[]=[];
 
   constructor(
-    private formBuilder:FormBuilder,
-    private Ss:ServiciosService, 
-    private router:Router,
-    private Cs:ContratoService ,
+    private formBuilder: FormBuilder,
+    private Ss: ServiciosService,
+    private router: Router,
+    private Cs: ContratoService,
     private route: ActivatedRoute
-){}
-ngOnInit(): void {
-  this.route.params.subscribe((data: Params) => {
-    this.id = data['id'];
-    this.edicion = data['id'] != null;
-    this.init();
-  });
-  
- this.form=this.formBuilder.group({
-  hcodigo: [''],
-hnombre:['',Validators.required],
-hdescripcion:['',Validators.required],
-hprecio:['',Validators.required],
-hcategoria:['',Validators.required],
-hestado:['',Validators.required],
-hcontrato:['',Validators.required],
-     }); 
-     this.Cs.list().subscribe(data=>{
-      this.listacontrato=data
-    })
-  }
-  aceptar():void{
-    if(this.form.valid){ 
-      this.servicios.id = this.form.value.hcodigo;  
-   this.servicios.nombre_servicio = this.form.value.hnombre;
-    this.servicios.descripcion = this.form.value.hdescripcion;
-    this.servicios.precio = this.form.value.hprecio;
-    this.servicios.categoria_servic = this.form.value.hcategoria;
-    this.servicios.estado_servic = this.form.value.hestado;
-    this.servicios.contrato.id = this.form.value.hcontrato;
-    if(this.edicion){   
-    this.Ss.update(this.servicios).subscribe((data)=>{
-      this.Ss.list().subscribe((data) => {
-        this.Ss.setList(data);
-      });
+  ) {}
+
+  ngOnInit(): void {
+    // Detecta si es edición o creación
+    this.route.params.subscribe((data: Params) => {
+      this.id = data['id'];
+      this.edicion = data['id'] != null;
+      this.init(); // Inicializa datos si es edición
     });
-  }else{
-    this.Ss.insert(this.servicios).subscribe((d) => {
-      this.Ss.list().subscribe((d) => {
-        this.Ss.setList(d);
-     });
+
+    // Inicializa el formulario
+    this.form = this.formBuilder.group({
+      hcodigo: [''],
+      hnombre: ['', Validators.required],
+      hdescripcion: ['', Validators.required],
+      hprecio: ['', [Validators.required, Validators.pattern('^[0-9]+(\\.[0-9]{1,2})?$')]], // Acepta números con 2 decimales
+      hcategoria: ['', Validators.required],
+      hestado: ['', Validators.required],
+      hcontrato: ['', Validators.required],
     });
-   }
+
+    // Carga los contratos
+    this.Cs.list().subscribe((data) => {
+      this.listacontrato = data;
+      console.log('Lista de contratos:', this.listacontrato); // Depuración
+    });
   }
-  this.router.navigate(['servicio'])
+
+  aceptar(): void {
+    if (this.form.valid) {
+      // Asigna valores al modelo `servicios`
+      this.servicios.id = this.form.value.hcodigo;
+      this.servicios.nombre_servicio = this.form.value.hnombre;
+      this.servicios.descripcion = this.form.value.hdescripcion;
+      this.servicios.precio = this.form.value.hprecio;
+      this.servicios.categoria_servic = this.form.value.hcategoria;
+      this.servicios.estado_servic = this.form.value.hestado;
+      this.servicios.contrato.id = this.form.value.hcontrato;
+
+      // Editar o registrar
+      if (this.edicion) {
+        this.Ss.update(this.servicios).subscribe(() => {
+          this.actualizarListaServicios();
+        });
+      } else {
+        this.Ss.insert(this.servicios).subscribe(() => {
+          this.actualizarListaServicios();
+        });
+      }
+      this.router.navigate(['servicio']);
+    } else {
+      console.error('Formulario inválido:', this.form.errors);
+    }
   }
-  init() {
-    if (this.edicion){
-      this.Ss.listId(this.id).subscribe((data)=> {
-        this.form = new FormGroup({
-          hcodigo: new FormControl(data.id),
-          hnombre: new FormControl(data.nombre_servicio),
-          hdescripcion: new FormControl(data.descripcion),
-          hprecio: new FormControl(data.precio),
-          hcategoria: new FormControl(data.categoria_servic),
-          hestado: new FormControl(data.estado_servic),
-          hcontrato: new FormControl(data.contrato.id),
+
+  init(): void {
+    if (this.edicion) {
+      this.Ss.listId(this.id).subscribe((data) => {
+        // Prellena el formulario con los datos del servicio
+        this.form = this.formBuilder.group({
+          hcodigo: [data.id],
+          hnombre: [data.nombre_servicio, Validators.required],
+          hdescripcion: [data.descripcion, Validators.required],
+          hprecio: [data.precio, Validators.required],
+          hcategoria: [data.categoria_servic, Validators.required],
+          hestado: [data.estado_servic, Validators.required],
+          hcontrato: [data.contrato?.id, Validators.required], // Asegura que contrato esté definido
         });
       });
     }
+  }
+
+  private actualizarListaServicios(): void {
+    this.Ss.list().subscribe((data) => {
+      this.Ss.setList(data); // Actualiza la lista de servicios en el servicio
+    });
   }
 }

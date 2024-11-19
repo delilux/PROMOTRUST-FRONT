b8dd9557d3fea35b1940c225432b9f6b936f2105
delilux <142470442+delilux@users.noreply.github.com>
2024-11-19 01:24:35 -0500

    deploy

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -23,7 +23,7 @@ export const appConfig: ApplicationConfig = {
         config: {
           tokenGetter: tokenGetter,
           allowedDomains: ['https://promotrustback-fff2c4gdh7dffpb2.canadacentral-01.azurewebsites.net/'],
-          disallowedRoutes: ['https://promotrustback-fff2c4gdh7dffpb2.canadacentral-01.azurewebsites.net/login/forget'],
+          disallowedRoutes: ['https://promotrustback-fff2c4gdh7dffpb2.canadacentral-01.azurewebsites.net//login/forget'],
         },
       })
     )

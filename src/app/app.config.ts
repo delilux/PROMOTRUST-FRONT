f6430f39a2089cfb4bf0ccd40ab601601cf47a43
delilux <142470442+delilux@users.noreply.github.com>
2024-11-19 03:28:37 -0500

    deploy

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,31 +1,49 @@
-import { ApplicationConfig, importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
+import {
+  ApplicationConfig,
+  importProvidersFrom,
+  provideZoneChangeDetection,
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
-import { provideHttpClient, withFetch, withInterceptorsFromDi } from '@angular/common/http';
+import {
+  provideHttpClient,
+  withFetch,
+  withInterceptorsFromDi,
+} from '@angular/common/http';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { JwtModule } from '@auth0/angular-jwt';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { TokenInterceptor } from './interceptors/token.interceptor';
+
 export function tokenGetter() {
   return sessionStorage.getItem('token');
 }
-
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideHttpClient(withFetch()),
-    provideHttpClient(withInterceptorsFromDi()), 
+    provideHttpClient(withInterceptorsFromDi()),
     provideAnimationsAsync(),
     importProvidersFrom(
       JwtModule.forRoot({
         config: {
           tokenGetter: tokenGetter,
-          allowedDomains: ['https://promotrustback-fff2c4gdh7dffpb2.canadacentral-01.azurewebsites.net'],
-          disallowedRoutes: ['https://promotrustback-fff2c4gdh7dffpb2.canadacentral-01.azurewebsites.net/login/forget'],
+          allowedDomains: [
+            'https://promotrustback-fff2c4gdh7dffpb2.canadacentral-01.azurewebsites.net',
+          ],
+          disallowedRoutes: [
+            'https://promotrustback-fff2c4gdh7dffpb2.canadacentral-01.azurewebsites.net/login/forget',
+          ],
         },
       })
-    )
+    ),
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: TokenInterceptor,
+      multi: true,
+    },
   ],
 };
